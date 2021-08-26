f9c8c7f00f342a99496757a9ce86122f0f345af7
Krystian <krystian.krecikk@wp.pl>
2021-08-27 00:47:36 +0200

    editing read me

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from MainWindow import Ui_MainWindow
 from PyQt5.QtWidgets import QLabel
 from PyQt5 import QtCore, QtWidgets
+from TicTacToe import TicTacToe
 import sys
 
 
@@ -13,23 +14,72 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.current_move = "X"
 
+        # tic tac toe logic class
+        self.tc = TicTacToe()
+
         # connect button
-        self.ui.r1_c1.clicked.connect(lambda: self.clicked_button(self.ui.r1_c1, "r1_c1"))
-        self.ui.r1_c2.clicked.connect(lambda: self.clicked_button(self.ui.r1_c2, "r1_c2"))
-        self.ui.r1_c3.clicked.connect(lambda: self.clicked_button(self.ui.r1_c3, "r1_c3"))
+        self.buttons = [self.ui.r1_c1, self.ui.r1_c2, self.ui.r1_c3,
+                        self.ui.r2_c1, self.ui.r2_c2, self.ui.r2_c3,
+                        self.ui.r3_c1, self.ui.r3_c2, self.ui.r3_c3]
 
-        self.ui.r2_c1.clicked.connect(lambda: self.clicked_button(self.ui.r2_c1, "r2_c1"))
-        self.ui.r2_c2.clicked.connect(lambda: self.clicked_button(self.ui.r2_c2, "r2_c2"))
-        self.ui.r2_c3.clicked.connect(lambda: self.clicked_button(self.ui.r2_c3, "r2_c3"))
+        self.ui.r1_c1.clicked.connect(lambda: self.clicked_button(self.ui.r1_c1, 0))
+        self.ui.r1_c2.clicked.connect(lambda: self.clicked_button(self.ui.r1_c2, 1))
+        self.ui.r1_c3.clicked.connect(lambda: self.clicked_button(self.ui.r1_c3, 2))
+        self.ui.r2_c1.clicked.connect(lambda: self.clicked_button(self.ui.r2_c1, 3))
+        self.ui.r2_c2.clicked.connect(lambda: self.clicked_button(self.ui.r2_c2, 4))
+        self.ui.r2_c3.clicked.connect(lambda: self.clicked_button(self.ui.r2_c3, 5))
+        self.ui.r3_c1.clicked.connect(lambda: self.clicked_button(self.ui.r3_c1, 6))
+        self.ui.r3_c2.clicked.connect(lambda: self.clicked_button(self.ui.r3_c2, 7))
+        self.ui.r3_c3.clicked.connect(lambda: self.clicked_button(self.ui.r3_c3, 8))
 
-        self.ui.r3_c1.clicked.connect(lambda: self.clicked_button(self.ui.r3_c1, "r3_c1"))
-        self.ui.r3_c2.clicked.connect(lambda: self.clicked_button(self.ui.r3_c2, "r3_c2"))
-        self.ui.r3_c3.clicked.connect(lambda: self.clicked_button(self.ui.r3_c3, "r3_c3"))
+        # Reset button
+        self.ui.reset_button.clicked.connect(self.reset_button)
 
-    def clicked_button(self, btn, text):
-        btn.setText(self.current_move)
-        btn.setEnabled(False)
-        self.switch()
+        # Status bar
+        self.bar_label = QLabel("Welcome")
+        self.statusBar().setStyleSheet("QLabel{font-weight:bold;color:grey}QStatusBar{border :1px solid gray;padding-left:8px;background:rgba(0,0,0,0);color:black;font-weight:bold;}")
+        self.statusBar().addPermanentWidget(self.bar_label)
+
+    def reset_button(self):
+        for button in self.buttons:
+            button.setEnabled(True)
+            button.setText("")
+        self.current_move = "X"
+        self.ui.current_move.setText(self.current_move)
+        for btn in self.buttons:
+            btn.setStyleSheet("")
+        self.tc.reset_moves()
+        self.bar("Reset")
+
+    def bar(self, text):
+        self.bar_label.setText(text)
+
+    def clicked_button(self, btn, position):
+        if self.tc.is_move_possible(position):
+            btn.setText(self.current_move)
+            self.tc.set_move(position, self.current_move)
+            btn.setEnabled(False)
+            win_state = self.tc.check_if_winning_position()
+            if win_state[0]:
+                self.bar(f"Win {self.current_move}")
+                self.end_of_game(win_state[1])
+                return
+            self.switch()
+        else:
+            self.bar("an impossible move")
+
+    def end_of_game(self, winning_position):
+        for position in winning_position:
+            self.buttons[position].setStyleSheet("QPushButton{border: 4px solid lightgreen;}")
+            self.statusBar().setStyleSheet(
+                "QLabel{font-weight:bold;color:grey}QStatusBar{border :1px solid gray;padding-left:8px;background:rgba(0,0,0,0);color:black;font-weight:bold;}")
+
+        self.set_off_buttons()
+        print(winning_position)
+
+    def set_off_buttons(self):
+        for btn in self.buttons:
+            btn.setEnabled(False)
 
     def change_current(self):
         self.ui.current_move.setText(self.current_move)
--- TicTacToe.py
+++ TicTacToe.py
@@ -1,30 +1,33 @@
 class TicTacToe:
     def __init__(self):
-        self.moves = ["", "", "",
-                      "", "", "",
-                      "", "", ""]
+        self.reset_moves()
 
-
-    def check_if_winning_position(self) -> bool:
+    def check_if_winning_position(self) -> (bool, []):
         winning_positions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
         for X_O in ["X", "O"]:
-            tmp = True
             for position in winning_positions:
+                tmp = True
                 for pos in position:
                     if self.moves[pos] != X_O:
                         tmp = False
                 if tmp:
-                    return True
-        return False
+                    return True, position
+        return False, []
 
     def set_move(self, position, who):
         self.moves[position] = who
 
     def print_board(self):
-        for row in self.moves:
-            print(row+str(1))
-tc = TicTacToe()
-tc.print_board()
+        for i, row in enumerate(self.moves):
+            move = 0 if row == "" else row
+            print(move, end=' ') if ((i+1) % 3 != 0) else print(move)
+        print("-----")
 
-print(tc.check_if_winning_position())
+    def is_move_possible(self, position):
+        return True if self.moves[position] == "" else False
+
+    def reset_moves(self):
+        self.moves = ["", "", "",
+                      "", "", "",
+                      "", "", ""]
