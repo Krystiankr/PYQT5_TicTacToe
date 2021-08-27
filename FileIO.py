15980493302830a1ea38cccdf617f7e0a96434c5
Krystian <krystian.krecikk@wp.pl>
2021-08-27 03:19:05 +0200

    update code add addition load scores from json file

---
+++ FileIO.py
@@ -0,0 +1,44 @@
+import json
+
+class File_Json():
+    def __init__(self):
+        try:
+            with open("data.json") as f:
+                self.data = json.load(f)
+        except Exception:
+            pass
+
+    def load(self):
+        print("xxx")
+        for element in self.data['Points']:
+            print(element, end=' value:')
+            print(self.data['Points'][element])
+        return self.data['Points']
+
+    def refresh_data(self, what_XO, count):
+        with open("data.json") as f:
+            data = json.load(f)
+            print(data)
+        data['Points'][what_XO] = count
+
+        with open("data.json", "w") as f:
+            json.dump(data, f)
+
+    def increase_xo_1(self, what_XO):
+        with open("data.json") as f:
+            data = json.load(f)
+            print(data)
+        data['Points'][what_XO] += 1
+
+        with open("data.json", "w") as f:
+            json.dump(data, f)
+
+    def save(self):
+        data = {}
+        data['Points'] = {
+            "X": 0,
+            "O": 3
+        }
+        with open("data.json", "w") as f:
+            json.dump(data, f)
+
