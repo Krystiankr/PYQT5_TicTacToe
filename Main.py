7988ea7a3a758c75cd7b9c554ca2155db27fb949
Krystian <krystian.krecikk@wp.pl>
2021-08-27 03:52:36 +0200

    added some comments

--- Main.py
+++ Main.py
@@ -16,13 +16,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.current_move = "X"
 
-        # tic tac toe logic class
+        # Tic Tac Toe logic class
         self.tc = TicTacToe()
 
-        # load scores
+        # We have to create a class every time.
         self.load_scores()
 
-        # connect button
+        # Redirection of the button signal to its own function
         self.buttons = [self.ui.r1_c1, self.ui.r1_c2, self.ui.r1_c3,
                         self.ui.r2_c1, self.ui.r2_c2, self.ui.r2_c3,
                         self.ui.r3_c1, self.ui.r3_c2, self.ui.r3_c3]
@@ -36,18 +36,15 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.r3_c1.clicked.connect(lambda: self.clicked_button(self.ui.r3_c1, 6))
         self.ui.r3_c2.clicked.connect(lambda: self.clicked_button(self.ui.r3_c2, 7))
         self.ui.r3_c3.clicked.connect(lambda: self.clicked_button(self.ui.r3_c3, 8))
-
-        # Reset button
         self.ui.reset_button.clicked.connect(self.reset_button)
 
-        # Status bar
+        # Status bar, it informs you about current changes
         self.bar_label = QLabel("Welcome")
         self.statusBar().setStyleSheet("QLabel{font-weight:bold;color:grey}QStatusBar{border :1px solid gray;padding-left:8px;background:rgba(0,0,0,0);color:black;font-weight:bold;}")
         self.statusBar().addPermanentWidget(self.bar_label)
 
     def load_scores(self):
         file = File_Json()
-
         data = file.load() #output dict
         scores = [self.ui.points_x, self.ui.points_o]
         for label, text in zip(scores, data.values()):
@@ -82,32 +79,29 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.bar(f"Win {self.current_move}")
                 self.end_of_game(win_state[1], self.current_move)
                 return
-            self.switch()
+            self.switch_current_move()
         else:
             self.bar("an impossible move")
 
     def end_of_game(self, winning_position, who):
-
         for position in winning_position:
             self.buttons[position].setStyleSheet("QPushButton{border: 4px solid lightgreen;}")
             self.statusBar().setStyleSheet(
                 "QLabel{font-weight:bold;color:grey}QStatusBar{border :1px solid gray;padding-left:8px;background:rgba(0,0,0,0);color:black;font-weight:bold;}")
 
-        self.set_off_buttons()
-        print(winning_position)
-        print(who)
+        self.turn_off_buttons()
         self.update_scores(who)
 
-    def set_off_buttons(self):
+    def turn_off_buttons(self):
         for btn in self.buttons:
             btn.setEnabled(False)
 
-    def change_current(self):
+    def change_display_current_move(self):
         self.ui.current_move.setText(self.current_move)
 
-    def switch(self):
+    def switch_current_move(self):
         self.current_move = "O" if self.current_move == "X" else "X"
-        self.change_current()
+        self.change_display_current_move()
         return self.current_move
 
 if __name__ == "__main__":
